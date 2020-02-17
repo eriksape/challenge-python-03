1d29d0b27fc9f7a392d930b4290b4de9e5e49ed7
eriksape <erik.sape@gmail.com>
2020-02-16 18:35:25 -0600

    solving challenge

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import os
+import re
 # Resolve the problem!!
 
 
 def run():
     # Start coding here
+    with open(os.getcwd() + '/src/encoded.txt', 'r', encoding='utf-8') as f:
+        read_data = f.read()
+        f.close()
+    text = re.split("[^a-zA-Z]*", read_data)
+    print(''.join(text))
 
 
 if __name__ == '__main__':
